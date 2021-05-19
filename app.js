a4c0ba397b94d708bd2048108e73a9466349bc74
Dheeraj Sharma <dhiru3599@gmail.com>
2021-05-19 16:45:56 +0530

    feat: add translate button

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 alert("js file activated");
 console.log("js file attached");
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", () => console.log("clicked!!!"));
